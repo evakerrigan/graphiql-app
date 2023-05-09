2ade57a9138307e40e9a855551fd037ece6a82f4
Eva Kerrigan <evakerrigan@gmail.com>
2023-05-09 04:50:04 +0300

    feat: add logo at header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,9 @@ import './Header.css';
 export const Header = () => {
   return (
     <div className="header">
-      <div className="header-logo">Логотип</div>
+      <a className="header-logo" href="./">
+        <img src="image/logo.jpg" className="header-img" />
+      </a>
       <button className="header-login">Login</button>
     </div>
   )
