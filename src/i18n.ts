5a65e30a55d960e44f5172cdffa7e2ae854eac1c
Eva Kerrigan <evakerrigan@gmail.com>
2023-05-28 23:25:32 +0300

    feat: start localization task

---
+++ src/i18n.ts
@@ -0,0 +1,22 @@
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import translationEn from './locales/en/translation.json';
+import translationRu from './locales/ru/translation.json';
+
+const resources = {
+  en: {
+    translation: translationEn,
+  },
+  ru: {
+    translation: translationRu,
+  },
+};
+
+i18next.use(initReactI18next).init({
+  resources,
+  lng: 'ru',
+  returnEmptyString: false,
+});
+
+
+export default i18next;
