318a7f20a29c6ae233d9ee7f03aa4e666f64d170
Eva Kerrigan <evakerrigan@gmail.com>
2023-05-28 23:37:58 +0300

    refactor: ts edits

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,13 +4,14 @@ import { auth } from "../../firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import "./Login.css";
-import { useReg } from "../../hooks/useReg";
+// import { useReg } from "../../hooks/useReg";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [user, loading, error] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
+  // const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
-  const isReg = useReg();
+  // const isReg = useReg();
 
   useEffect(() => {
     if (loading) {
@@ -18,24 +19,26 @@ function Login() {
       return;
     }
     if (user) navigate("/main");
-  }, [user, loading]);
+  }, [user, loading, navigate]);
 
 
   // async const authTrue = () => {
   //   console.log('проверка на запуск isReg.user', isReg.user);
   //   await signInWithEmailAndPassword(auth, email, password);
   //   if (isReg.user) navigate("/main");
+  // // }
+
+
+  // const authTrue = async () => {
+  //   console.log('проверка на запуск isReg.user', isReg.user);
+  //   try {
+  //     await signInWithEmailAndPassword(auth, email, password);
+  //     console.log('user', user)
+  //     if (user) navigate("/main");
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
   // }
-  const authTrue = async () => {
-    console.log('проверка на запуск isReg.user', isReg.user);
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-      console.log('user', user)
-      if (user) navigate("/main");
-    } catch (error) {
-      console.error(error);
-    }
-  }
 
   return (
     <div className="login">
